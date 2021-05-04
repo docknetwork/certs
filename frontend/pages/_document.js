a863933843e3181e7a57a7825638c597de0e83f3
Samuel Hellawell <sshellawell@gmail.com>
2021-05-04 17:17:21 +0100

    fix static icon path
    Signed-off-by: Samuel Hellawell <sshellawell@gmail.com>

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -33,7 +33,7 @@ export default class MyDocument extends Document {
           <meta name="theme-color" content="#fff" />
 
           {/* Progressive Web App: Provide manifest file for metadata */}
-          <link rel="manifest" href="./static/manifest.json" />
+          <link rel="manifest" href="/static/manifest.json" />
 
           {/* Roboto and icons fonts */}
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
@@ -59,60 +59,60 @@ export default class MyDocument extends Document {
           <link
             rel="apple-touch-icon"
             sizes="57x57"
-            href="./static/graphics/icon-57.png"
+            href="/static/graphics/icon-57.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="72x72"
-            href="./static/graphics/icon-72.png"
+            href="/static/graphics/icon-72.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="114x114"
-            href="./static/graphics/icon-114.png"
+            href="/static/graphics/icon-114.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="144x144"
-            href="./static/graphics/icon-144.png"
+            href="/static/graphics/icon-144.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="512x512"
-            href="./static/graphics/icon-512.png"
+            href="/static/graphics/icon-512.png"
           />
           <link
-            href="./static/graphics/splash-2048.png"
+            href="/static/graphics/splash-2048.png"
             media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="./static/graphics/splash-1668.png"
+            href="/static/graphics/splash-1668.png"
             media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="./static/graphics/splash-1536.png"
+            href="/static/graphics/splash-1536.png"
             media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="./static/graphics/splash-1125.png"
+            href="/static/graphics/splash-1125.png"
             media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="./static/graphics/splash-1242.png"
+            href="/static/graphics/splash-1242.png"
             media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="./static/graphics/splash-750.png"
+            href="/static/graphics/splash-750.png"
             media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
             rel="apple-touch-startup-image"
           />
           <link
-            href="./static/graphics/splash-640.png"
+            href="/static/graphics/splash-640.png"
             media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
             rel="apple-touch-startup-image"
           />
